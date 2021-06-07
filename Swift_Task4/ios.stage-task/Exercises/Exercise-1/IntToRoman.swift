47b49251896b053f860e3cb3a3e9f06966b19f8d
Tatyana Luzanova <82838416+Tane4ek@users.noreply.github.com>
2021-06-07 22:06:29 +0300

    Update IntToRoman.swift
    docs: add solution of exercise 1

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,21 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+        let romanToInt: [Int : String] = [1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M"]
+        var intNumbers = [Int](romanToInt.keys)
+        intNumbers.sort(by: {$0 > $1})
+        
+        var outputString = String()
+        var value = self
+        if (self <= 0 || self > 3999) {
+            return nil
+        }
+        for i in 0 ..< intNumbers.count {
+            while value >= intNumbers[i] {
+                value -= intNumbers[i]
+                outputString += romanToInt[intNumbers[i]]!
+            }
+        }
+       return outputString
     }
 }
